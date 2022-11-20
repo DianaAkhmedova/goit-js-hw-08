fd4e6d5a9ab7b251568f33bc9608babb6ca807cb
DianaAkhmedova <106441711+DianaAkhmedova@users.noreply.github.com>
2022-11-20 22:29:18 +0200

    add gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const refs = {
+  gallery: document.querySelector('.gallery'),
+};
+
+const markup = galleryItems
+  .map(
+    item => `<a class="gallery__item" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+      </a>`
+  )
+  .join('');
+
+refs.gallery.innerHTML = markup;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
